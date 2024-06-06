08a7f2c74157d4fd92b92fbcffa989e45fb857b5
pedrohfideeelis <phzimnerdola@gmail.com>
2024-06-06 16:32:55 -0300

    update cardapio_component

--- src/components/CardapioComponent/Cardapio.jsx
+++ src/components/CardapioComponent/Cardapio.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import "./styles.css";
+import "./styles.css"
 import { CardapioCard } from "../CardapioCardComponent/CardapioCard";
 
 export class Cardapio extends React.Component{
     render(){
         return(
-            <section id="pizza-cards">
-                <CardapioCard/>
-            </section>
+            <div className="cardapio-container">
+                <h3>Cardápio</h3>
+                <section id="cardapio-cards">
+                    <CardapioCard/>
+                    <CardapioCard/>
+                    <CardapioCard/>
+                    <CardapioCard/>
+                    <CardapioCard/>
+                    <CardapioCard/>
+                    <CardapioCard/>
+                    <CardapioCard/>
+                    <CardapioCard/>
+                </section>
+            </div>
         );
     }
 }
