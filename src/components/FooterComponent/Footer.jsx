d22b4659f09ef983a027c94222a1872a843c401f
pedrohfideeelis <phzimnerdola@gmail.com>
2024-06-06 10:06:29 -0300

    update footer_component

--- src/components/FooterComponent/Footer.jsx
+++ src/components/FooterComponent/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import LogoImg from "../../assets/images/PIZZA_LOGO.png";
+import LogoImg from "../../assets/images/PIZZA_LOGO_HORIZONTAL.png";
 import WhatsApp from "../../assets/images/icons/whatsapp.png";
 import Instagram from "../../assets/images/icons/instagram.png";
 import Telefone from "../../assets/images/icons/telefone.png";
@@ -15,75 +15,46 @@ class Footer extends React.Component {
           <div id="site-map">
             <div className="footer-section">
               <h4>Menu</h4>
-              <ul id="menu">
-                <li id="map-link">
-                  <a href="/">Promoções</a>
-                </li>
-                <li id="map-link">
-                  <a href="/">Cardápio</a>
-                </li>
-                <li id="map-link">
-                  <a href="/">Sobre Nós</a>
-                </li>
+              <ul>
+                <li><a href="/">Promoções</a></li>
+                <li><a href="/">Cardápio</a></li>
+                <li><a href="/">Sobre Nós</a></li>
               </ul>
             </div>
             <div className="footer-section">
               <h4>Cardápio</h4>
-              <ul id="cardapio">
-                <li id="map-link">
-                  <a href="/">Pizzas</a>
-                </li>
+              <ul>
+                <li><a href="/">Pizzas</a></li>
               </ul>
             </div>
             <div className="footer-section">
               <h4>Sobre Nós</h4>
-              <ul id="sobre">
-                <li id="map-link">
-                  <a href="/">Nossa História</a>
-                </li>
-                <li id="map-link">
-                  <a href="/">Nossa Equipe</a>
-                </li>
-                <li id="map-link">
-                  <a href="/">A Pizzaria</a>
-                </li>
+              <ul>
+                <li><a href="/">Nossa História</a></li>
+                <li><a href="/">Nossa Equipe</a></li>
+                <li><a href="/">A Pizzaria</a></li>
               </ul>
             </div>
             <div className="footer-section">
               <h4>Termos</h4>
-              <ul id="termos">
-                <li>
-                  <a href="/">Política de Privacidade</a>
-                </li>
-                <li>
-                  <a href="/">Termos de Uso</a>
-                </li>
+              <ul>
+                <li><a href="/">Política de Privacidade</a></li>
+                <li><a href="/">Termos de Uso</a></li>
               </ul>
             </div>
           </div>
 
-          <div className="footer-brand">
-            <a href="/">
-              <img src={LogoImg} alt="Pizza Logo" />
-            </a>
-          </div>
-
-          <div id="contact">
-            <a href="/" id="contact-item">
-              <img src={WhatsApp} alt="WhatsApp" />
-            </a>
-            <a href="/" id="contact-item">
-              <img src={Instagram} alt="Instagram" />
-            </a>
-            <a href="/" id="contact-item">
-              <img src={Telefone} alt="Telefone" />
-            </a>
-            <a href="/" id="contact-item">
-              <img src={Email} alt="Email" />
-            </a>
-            <a href="/" id="contact-item">
-              <img src={Local} alt="Local" />
-            </a>
+          <div id="contact-brand">
+            <div className="footer-brand">
+              <a href="/"><img src={LogoImg} alt="Pizza Logo" /></a>
+            </div>
+            <div id="contact">
+              <a href="/"><img src={WhatsApp} alt="WhatsApp" /></a>
+              <a href="/"><img src={Instagram} alt="Instagram" /></a>
+              <a href="/"><img src={Telefone} alt="Telefone" /></a>
+              <a href="/"><img src={Email} alt="Email" /></a>
+              <a href="/"><img src={Local} alt="Local" /></a>
+            </div>
           </div>
         </div>
 
