59e8fe8f020a4a694b88f717159791e6b092e3e2
pedrohfideeelis <phzimnerdola@gmail.com>
2024-06-06 14:17:10 -0300

    add routes cardapio_preview

--- src/components/CardapioPreviewComponent/CardapioPreview.jsx
+++ src/components/CardapioPreviewComponent/CardapioPreview.jsx
@@ -9,6 +9,7 @@ import SashimiImg from "../../assets/images/pizzas/sashimi.jpeg";
 import SushiImg from "../../assets/images/pizzas/sushi.jpeg";
 import VegImg from "../../assets/images/pizzas/vegetariana.jpeg";
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 class CardapioPreview extends React.Component {
   render() {
@@ -18,39 +19,57 @@ class CardapioPreview extends React.Component {
         <div id="pizza-cards">
           <div className="pizza-card">
             <img src={AtumImg} alt="Pizza de Atum" />
-            <button>PEÇA AGORA</button>
+            <Link to="/cardapio">
+              <button>PEÇA AGORA</button>
+            </Link>
           </div>
           <div className="pizza-card">
             <img src={CarneSecaImg} alt="Pizza de Carne Seca" />
-            <button>PEÇA AGORA</button>
+            <Link to="/cardapio">
+              <button>PEÇA AGORA</button>
+            </Link>
           </div>
           <div className="pizza-card">
             <img src={FrangoImg} alt="Pizza de Frango" />
-            <button>PEÇA AGORA</button>
+            <Link to="/cardapio">
+              <button>PEÇA AGORA</button>
+            </Link>
           </div>
           <div className="pizza-card">
             <img src={GuiozaImg} alt="Pizza de Guioza" />
-            <button>PEÇA AGORA</button>
+            <Link to="/cardapio">
+              <button>PEÇA AGORA</button>
+            </Link>
           </div>
           <div className="pizza-card">
             <img src={PepperoniImg} alt="Pizza de Pepperoni" />
-            <button>PEÇA AGORA</button>
+            <Link to="/cardapio">
+              <button>PEÇA AGORA</button>
+            </Link>
           </div>
           <div className="pizza-card">
             <img src={QueijoImg} alt="Pizza de Queijo" />
-            <button>PEÇA AGORA</button>
+            <Link to="/cardapio">
+              <button>PEÇA AGORA</button>
+            </Link>
           </div>
           <div className="pizza-card">
             <img src={SashimiImg} alt="Pizza de Sashimi" />
-            <button>PEÇA AGORA</button>
+            <Link to="/cardapio">
+              <button>PEÇA AGORA</button>
+            </Link>
           </div>
           <div className="pizza-card">
             <img src={SushiImg} alt="Pizza de Sushi" />
-            <button>PEÇA AGORA</button>
+            <Link to="/cardapio">
+              <button>PEÇA AGORA</button>
+            </Link>
           </div>
           <div className="pizza-card">
             <img src={VegImg} alt="Pizza Vegetariana" />
-            <button>PEÇA AGORA</button>
+            <Link to="/cardapio">
+              <button>PEÇA AGORA</button>
+            </Link>
           </div>
         </div>
       </section>
