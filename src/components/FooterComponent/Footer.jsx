faeba22edb3739b30286a6ae569419b783386211
pedrohfideeelis <phzimnerdola@gmail.com>
2024-06-13 15:46:35 -0300

    adjusts and update footer

--- src/components/FooterComponent/Footer.jsx
+++ src/components/FooterComponent/Footer.jsx
@@ -6,6 +6,7 @@ import Instagram from "../../assets/images/icons/instagram.png";
 import Telefone from "../../assets/images/icons/telefone.png";
 import Email from "../../assets/images/icons/e-mail.png";
 import Local from "../../assets/images/icons/local.png";
+import { Link } from "react-router-dom";
 
 export class Footer extends React.Component {
   render() {
@@ -16,30 +17,30 @@ export class Footer extends React.Component {
             <div className="footer-section">
               <h4>Menu</h4>
               <ul>
-                <li><a href="/">Promoções</a></li>
-                <li><a href="/">Cardápio</a></li>
-                <li><a href="/">Sobre Nós</a></li>
+                <li><Link to="/">Promoções</Link></li>
+                <li><Link to="/cardapio">Cardápio</Link></li>
+                <li><Link to="/sobre-nos">Sobre Nós</Link></li>
               </ul>
             </div>
             <div className="footer-section">
               <h4>Cardápio</h4>
               <ul>
-                <li><a href="/">Pizzas</a></li>
+                <li><Link href="/cardapio">Pizzas</Link></li>
               </ul>
             </div>
             <div className="footer-section">
               <h4>Sobre Nós</h4>
               <ul>
-                <li><a href="/">Nossa História</a></li>
-                <li><a href="/">Nossa Equipe</a></li>
-                <li><a href="/">A Pizzaria</a></li>
+                <li><Link href="/sobre-nos">Nossa História</Link></li>
+                <li><Link href="/sobre-nos">Nossa Equipe</Link></li>
+                <li><Link href="/sobre-nos">A Pizzaria</Link></li>
               </ul>
             </div>
             <div className="footer-section">
               <h4>Termos</h4>
               <ul>
-                <li><a href="/">Política de Privacidade</a></li>
-                <li><a href="/">Termos de Uso</a></li>
+                <li><Link href="/termos">Política de Privacidade</Link></li>
+                <li><Link href="/termos">Termos de Uso</Link></li>
               </ul>
             </div>
           </div>
@@ -49,8 +50,8 @@ export class Footer extends React.Component {
               <a href="/"><img src={LogoImg} alt="Pizza Logo" /></a>
             </div>
             <div id="contact">
-              <a href="/"><img src={WhatsApp} alt="WhatsApp" /></a>
-              <a href="/"><img src={Instagram} alt="Instagram" /></a>
+              <a href="https://wa.me/5562994500960"><img src={WhatsApp} alt="WhatsApp" /></a>
+              <a href="/https://instagram.com/__uaifideeelis"><img src={Instagram} alt="Instagram" /></a>
               <a href="/"><img src={Telefone} alt="Telefone" /></a>
               <a href="/"><img src={Email} alt="Email" /></a>
               <a href="/"><img src={Local} alt="Local" /></a>
